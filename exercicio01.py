68c48ae18962bc359a5c1ef9da9e6d6f934a17c0
DEH135 <deboraalvesdecastro896@gmail.com>
2026-05-12 17:46:00 -0300

    meu primeiro programa

---
+++ exercicio01.py
@@ -0,0 +1,26 @@
+n1 = int(input("digite 01 n"))
+n2 = int(input("digite 01 n"))
+
+resultado = n1 + n2
+resultado2 = n1 % n2
+resultado3 = n1 ** n2
+
+print("o resultado e1 : " , resultado)
+
+print("o resultado da parte inteira da divisão e : ", resultado)
+print("o resultado2 do resto da divisão e : " , resultado2)
+print("o resultado da potencia e : " , resultado3)
+
+print("-----------------------------------")
+print("  OPERADORES RELACIONAIS   ")
+
+
+relacao1 = n1 > n2
+relacao2 = n1 < n2
+relacao3 = n1 < n2
+relacao4 = n1 <= n2 
+relacao5 = n1 == n2
+relacao6 = n1 != n2
+
+
+print("os resultados das relacao estarao abaixo: \n{} \n {} \n {} \n{} \n{} \n{}".format(relacao1, relacao2, relacao3, relacao4,relacao5, relacao6))
---
+++ progama01.py
@@ -0,0 +1,18 @@
+print("hello wolrd") 
+ptint("1") # str (string de caracteres)
+print(1) # int (numero inteiro)
+print(-2) # int (numero inteiro)
+print(8.2) # float (numero real ou decimal, ponto flutuante)
+print(True) # verdadeiro (booleano -boool)
+print(False) # falso (booleano - bool)
+
+nome = "Maria"
+idade = 19
+peso = 62.5
+maioridade = True
+
+
+print( "essa e ", nome, ", ela tem ," idade ", anos e pesa ," peso ",)
+
+print("o  nome de la e " + nome " eu encontreiela no tinder")
+print(hoje e aniversario da {} e ela completa{} de idade e pesa {} Kg, alem diso ela e maior de idade: {}". format(nome, idae, peso, maioridade)")
