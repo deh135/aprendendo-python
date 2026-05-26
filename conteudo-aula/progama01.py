a4781a7602046b10390cb646b58b6965480b5597
deh135 <deboraalvesdecastro896@gmail.com>
2026-05-26 17:11:03 -0300

    Update progama01.py

--- conteudo-aula/progama01.py
+++ conteudo-aula/progama01.py
@@ -3,7 +3,7 @@ print("1") # str (string de caracteres)
 print(1) # int (numero inteiro)
 print(-2) # int (numero inteiro)
 print(8.2) # float (numero real ou decimal, ponto flutuante)
-print(True) # verdadeiro (booleano -boool)
+print(True) # verdadeiro (booleano -bool)
 print(False) # falso (booleano - bool)
 
 nome = 
