3c190e80fc1ea80b98265a0622e162d64393a3e8
deh135 <deboraalvesdecastro896@gmail.com>
2026-05-26 17:46:44 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+n1 = int(input("digite o primeiro numero: "))
+n2 = int(input("digite o segundo numero: "))
+
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+
+print("soma:", soma)
+print("subtracao:", subtracao)
+print("multiplicacao:", multiplicacao)
+print("divisao:", divisao)
---
+++ exercicio14.py
@@ -0,0 +1,30 @@
+while True:
+    print("1 - somar")
+    print("2 - subtrair")
+    print("3 - multiplicar")
+    print("4 - dividir")
+    print("5 - sair")
+
+    opcao = int(input("escolha uma opcao?"))
+
+    if opcao ==  5:
+        print("progama encerrado")
+        break
+
+    n1 = int(input("digite o primeiro numero: "))
+    n2 = int(input("digite o segundo numero: "))
+
+    if opcao == 1:
+        print("resultado: ", n1 + n2)
+
+    elif opcao == 2:
+        print("resultado: ", n1 - n2)
+
+    elif opcao == 3:
+       print("resultado: ", n1 * n2)
+
+    elif opcao == 4:
+        print("resultado: ", n1 / n2)
+
+    else:
+        print("opcao invalida")
